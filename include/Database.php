cd45d7385038fb4ebec3bae55f990445bdc79196
M <62713301+mejdanek@users.noreply.github.com>
2022-06-26 12:17:25 +0200

    Create Database.php

---
+++ include/Database.php
@@ -0,0 +1,22 @@
+<?php
+
+class Database
+{
+    
+    private $host = "localhost";
+    private $db_name = "biblioteca";
+    private $username = "root";
+    private $password = "root";
+    private $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name . ";charset=utf8", $this->username, $this->password);
+        } catch (PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
